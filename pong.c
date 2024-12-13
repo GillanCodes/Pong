57ff2497c38c9de70600be541c2fa804f9189433
gillancodes <gillancodes@laposte.net>
2024-12-13 16:48:11 +0100

    feat: added a small rectangle

--- pong.c
+++ pong.c
@@ -1,8 +1,18 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
+#include <SDL2/SDL_surface.h>
 
 int main()
 {
   SDL_InitSubSystem(SDL_INIT_VIDEO);
-  SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
-  SDL_Delay(10000);
+  SDL_Window *window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
+  SDL_Surface *surface = SDL_GetWindowSurface(window);
+  
+  SDL_Rect rect = (SDL_Rect) {0, 40, 40, 40};
+  Uint32 color = 0xFFFFFFFF;
+  SDL_FillRect(surface, &rect, color);
+  SDL_UpdateWindowSurface(window);
+
+  SDL_Delay(5000);
+
 }
