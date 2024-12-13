fc2437a24f6d470036870e6c0c493f3a1ab4a38c
gillancodes <gillancodes@laposte.net>
2024-12-13 16:54:07 +0100

    feat: added 2nd player rect

--- pong.c
+++ pong.c
@@ -8,9 +8,14 @@ int main()
   SDL_Window *window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
   SDL_Surface *surface = SDL_GetWindowSurface(window);
   
-  SDL_Rect rect = (SDL_Rect) {0, 40, 40, 40};
-  Uint32 color = 0xFFFFFFFF;
-  SDL_FillRect(surface, &rect, color);
+  Uint32 color = 0xFFFFFFFF;  
+
+  SDL_Rect pl1 = (SDL_Rect) {40, 40, 40, 200};
+  SDL_FillRect(surface, &pl1, color);
+ 
+  SDL_Rect pl2 = (SDL_Rect) {560, 120, 40, 200};
+  SDL_FillRect(surface, &pl2, color);
+
   SDL_UpdateWindowSurface(window);
 
   SDL_Delay(5000);
