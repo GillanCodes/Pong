2a9cf787b48265bb73b4a05380090c9981cd07b0
gillancodes <gillancodes@laposte.net>
2024-12-19 11:41:57 +0100

    feat: added ball

--- pong.c
+++ pong.c
@@ -1,12 +1,19 @@
 #include <SDL2/SDL.h>
 
-#define  MOVEMENT_SPEED 10
+#define MOVEMENT_SPEED 10
+#define WHITE_COLOR 0xFFFFFFFF
 
-void move_rect(SDL_Surface* surface, SDL_Rect * rect, int down)
+typedef struct Speed {
+  int x;
+  int y;
+} Speed;
+
+void move_rect(SDL_Surface* surface, SDL_Rect * rect, Speed *speed)
 {
   SDL_FillRect(surface, rect, 0x00000000);
-  rect->y += down;
-  SDL_FillRect(surface, rect, 0xFFFFFFFF);
+  rect->x += speed->x;
+  rect->y += speed->y;
+  SDL_FillRect(surface, rect, WHITE_COLOR);
 }
 
 int main()
@@ -15,13 +22,17 @@ int main()
   SDL_Window *window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
   SDL_Surface *surface = SDL_GetWindowSurface(window);
   
-  Uint32 color = 0xFFFFFFFF;  
-
   SDL_Rect pl1 = (SDL_Rect) {40, 40, 40, 200};
-  SDL_FillRect(surface, &pl1, color);
+  SDL_FillRect(surface, &pl1, WHITE_COLOR);
 
   SDL_Rect pl2 = (SDL_Rect) {560, 120, 40, 200};
-  SDL_FillRect(surface, &pl2, color);
+  SDL_FillRect(surface, &pl2, WHITE_COLOR);
+
+  SDL_Rect ball = (SDL_Rect) {320, 240, 10,10};
+  SDL_FillRect(surface, &ball, WHITE_COLOR);
+
+  SDL_Rect border = (SDL_Rect) {320, 0, 1, 480};
+  SDL_FillRect(surface, &border, WHITE_COLOR);
 
   int running = 1;
   SDL_Event event;
@@ -39,20 +50,24 @@ int main()
     {
       if (event.key.keysym.sym == SDLK_UP)
       {
-        move_rect(surface, &pl2, -MOVEMENT_SPEED);
+        Speed speed = (Speed) {0, -MOVEMENT_SPEED};
+        move_rect(surface, &pl2, &speed);
       }
       if (event.key.keysym.sym == SDLK_DOWN)
       {
-        move_rect(surface, &pl2, MOVEMENT_SPEED);
+        Speed speed = (Speed) {0, +MOVEMENT_SPEED};
+        move_rect(surface, &pl2, &speed);
       }
       
       if (event.key.keysym.sym == SDLK_z)
       {
-        move_rect(surface, &pl1, -MOVEMENT_SPEED);
+        Speed speed = (Speed) {0, -MOVEMENT_SPEED};
+        move_rect(surface, &pl1, &speed);
       }
       if (event.key.keysym.sym == SDLK_s)
       {
-        move_rect(surface, &pl1, MOVEMENT_SPEED);
+        Speed speed = (Speed) {0, +MOVEMENT_SPEED};
+        move_rect(surface, &pl1, &speed);
       }
     }
     
