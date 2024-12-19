c327d70704d043f76279004052740b5a7c949a55
gillancodes <gillancodes@laposte.net>
2024-12-19 14:17:37 +0100

    refactor: added comments

--- pong.c
+++ pong.c
@@ -19,6 +19,7 @@ typedef struct Speed {
   int y;
 } Speed;
 
+//Handle rectengle drawing
 void move_rect(SDL_Surface* surface, SDL_Rect* rect, Speed* speed)
 {
   SDL_FillRect(surface, rect, 0x00000000);
@@ -26,7 +27,7 @@ void move_rect(SDL_Surface* surface, SDL_Rect* rect, Speed* speed)
   rect->y += speed->y;
   SDL_FillRect(surface, rect, WHITE_COLOR);
 }
-
+// Handle player movement
 /* direction is 1 or -1 */
 void move_player(SDL_Surface* surface, SDL_Rect* pl,int direction)
 {
@@ -38,8 +39,10 @@ void move_player(SDL_Surface* surface, SDL_Rect* pl,int direction)
   move_rect(surface, pl ,&speed);
 }
 
+// Handle Ball mouvement
 void move_ball(SDL_Surface* surface, SDL_Rect* ball, SDL_Rect* pl1, SDL_Rect* pl2, Speed* ball_speed)
 {
+  //When hit Player 1 side
   if (ball->x <= LEFT_INNER_BORDER)
   {
     if ((ball->y + ball->h > pl1->y) && (ball->y < pl1->y + pl1->h))
@@ -49,7 +52,8 @@ void move_ball(SDL_Surface* surface, SDL_Rect* ball, SDL_Rect* pl1, SDL_Rect* pl
       ball_speed->y = (double) (hit_fraction * (double) MOVEMENT_SPEED);
     }  
   }
-
+  
+  //When hit player 2 side
   if (ball->x + ball->w >= RIGHT_INNER_BORDER)
   {
     if ((ball->y + ball->h > pl2->y) && (ball->y < pl2->y + pl2->h))
@@ -60,6 +64,7 @@ void move_ball(SDL_Surface* surface, SDL_Rect* ball, SDL_Rect* pl1, SDL_Rect* pl
     }
   }
 
+  //Ball hit top or bottom
   if(ball->y <= TOP_INNER_BORDER)
     ball_speed->y = -ball_speed->y;
   if(ball_speed->y + ball->h >= BOTTOM_INNER_BORDER)
@@ -69,36 +74,44 @@ void move_ball(SDL_Surface* surface, SDL_Rect* ball, SDL_Rect* pl1, SDL_Rect* pl
 
 int main()
 {
+  //Init the window with SDL2
   SDL_InitSubSystem(SDL_INIT_VIDEO);
   SDL_Window *window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
   SDL_Surface *surface = SDL_GetWindowSurface(window);
   
+  //Init Player 1
   SDL_Rect pl1 = (SDL_Rect) {LEFT_INNER_BORDER - PLAYER_WIDTH, 40, PLAYER_WIDTH, PLAYER_HEIGHT};
   SDL_FillRect(surface, &pl1, WHITE_COLOR);
 
+  //Init Player 2
   SDL_Rect pl2 = (SDL_Rect) {RIGHT_INNER_BORDER, 10, PLAYER_WIDTH, PLAYER_HEIGHT};
   SDL_FillRect(surface, &pl2, WHITE_COLOR);
 
+  //Init Ball
   SDL_Rect ball = (SDL_Rect) {(LEFT_INNER_BORDER + RIGHT_INNER_BORDER)/2, 10, BALL_DIAMETER, BALL_DIAMETER};
   SDL_FillRect(surface, &ball, WHITE_COLOR);
   Speed ball_speed = (Speed) {MOVEMENT_SPEED, 0};
 
+  //Init Border
   SDL_Rect border = (SDL_Rect) {320, 0, 1, 480};
 
   int running = 1;
   SDL_Event event;
 
+  // Running loop
   while (running)
   {
     SDL_PollEvent(&event);
-      
+    // Quit listener
     if (event.type == SDL_QUIT)
     {
       running = 0;
     }
 
+    //Input listener
     if (event.type == SDL_KEYDOWN)
     {
+      //Player 2 mouvements
       if (event.key.keysym.sym == SDLK_UP)
       {
         move_player(surface, &pl2, -1);
@@ -107,7 +120,7 @@ int main()
       {
         move_player(surface, &pl2, 1);
       }
-      
+      //Player 1 mouvements
       if (event.key.keysym.sym == SDLK_z)
       {
         move_player(surface, &pl1, -1);
@@ -124,6 +137,7 @@ int main()
     //Draw Border
     SDL_FillRect(surface, &border, WHITE_COLOR);
     
+    //Update window
     SDL_UpdateWindowSurface(window);
     SDL_Delay(10);
   }
