2c939ec822f7aafed99cc8e32fc62a613adebe66
gillancodes <gillancodes@laposte.net>
2024-12-19 12:22:42 +0100

    feat: added ball movements

--- pong.c
+++ pong.c
@@ -1,14 +1,22 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_rect.h>
 
-#define MOVEMENT_SPEED 10
 #define WHITE_COLOR 0xFFFFFFFF
+
+static int MOVEMENT_SPEED = 10;
+
+static int LEFT_INNER_BORDER = 80;
+static int RIGHT_INNER_BORDER = 560;
+
+static int PLAYER_WIDTH = 40;
+static int PLAYER_HEIGHT = 200;
 
 typedef struct Speed {
   int x;
   int y;
 } Speed;
 
-void move_rect(SDL_Surface* surface, SDL_Rect * rect, Speed *speed)
+void move_rect(SDL_Surface* surface, SDL_Rect* rect, Speed* speed)
 {
   SDL_FillRect(surface, rect, 0x00000000);
   rect->x += speed->x;
@@ -27,23 +35,33 @@ void move_player(SDL_Surface* surface, SDL_Rect* pl,int direction)
   move_rect(surface, pl ,&speed);
 }
 
+void move_ball(SDL_Surface* surface, SDL_Rect* ball, SDL_Rect* pl1, SDL_Rect* pl2, Speed* ball_speed)
+{
+  if (ball->x < LEFT_INNER_BORDER)
+    ball_speed->x = -ball_speed->x;
+  if (ball->x + ball->w > RIGHT_INNER_BORDER)
+    ball_speed->x = -ball_speed->x;
+
+  move_rect(surface, ball, ball_speed);
+}
+
 int main()
 {
   SDL_InitSubSystem(SDL_INIT_VIDEO);
   SDL_Window *window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, 0);
   SDL_Surface *surface = SDL_GetWindowSurface(window);
   
-  SDL_Rect pl1 = (SDL_Rect) {40, 40, 40, 200};
+  SDL_Rect pl1 = (SDL_Rect) {LEFT_INNER_BORDER - PLAYER_WIDTH, 40, PLAYER_WIDTH, PLAYER_HEIGHT};
   SDL_FillRect(surface, &pl1, WHITE_COLOR);
 
-  SDL_Rect pl2 = (SDL_Rect) {560, 120, 40, 200};
+  SDL_Rect pl2 = (SDL_Rect) {RIGHT_INNER_BORDER, 120, PLAYER_WIDTH, PLAYER_HEIGHT};
   SDL_FillRect(surface, &pl2, WHITE_COLOR);
 
   SDL_Rect ball = (SDL_Rect) {320, 240, 10,10};
   SDL_FillRect(surface, &ball, WHITE_COLOR);
+  Speed ball_speed = (Speed) {10, 0};
 
   SDL_Rect border = (SDL_Rect) {320, 0, 1, 480};
-  SDL_FillRect(surface, &border, WHITE_COLOR);
 
   int running = 1;
   SDL_Event event;
@@ -77,8 +95,14 @@ int main()
         move_player(surface, &pl2, 1);
       }
     }
+
+    //move ball
+    move_ball(surface, &ball, &pl1, &pl2, &ball_speed);
+   
+    //Draw Border
+    SDL_FillRect(surface, &border, WHITE_COLOR);
     
     SDL_UpdateWindowSurface(window);
-    SDL_Delay(1);
+    SDL_Delay(10);
   }
 }
